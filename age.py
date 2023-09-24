4b16020b530da0e0053f14bcb244ec2b16dfc0b3
HarvindG <131158546+HarvindG@users.noreply.github.com>
2023-09-24 20:23:44 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+year = int(input('Enter your year of birth: '))
+month = int(input('Enter your month of birth: '))
+day = int(input('Enter your day of birth: '))
+
+birthdate = datetime(year, month, day)
+
+current_date = datetime.now()
+
+age = current_date.year - birthdate.year - ((current_date.month, current_date.day) < (birthdate.month, birthdate.day))
+
+
+print('You are now ' + str(age) + ' year old')
---
+++ maxmin.py
@@ -0,0 +1,11 @@
+def outlier_num(my_list):
+  largest_num = float('-inf')
+  smallest_num = float('inf')
+  for nums in my_list:
+    if nums > largest_num:
+      largest_num = nums
+    if nums < smallest_num:
+      smallest_num = nums
+  return smallest_num, largest_num 
+
+print(outlier_num([20, 50, 12, 6, 14, 8]))
